2b54f9b45b6d4850c4aabd541a52d2ba114e15ef
shraddha_patade <shraddha.p@themailwhale.in>
2025-07-19 14:31:29 +0530

    first layout

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-function toggleNested(row) {
-  let next = row.nextElementSibling;
-  while (next && next.classList.contains("nested-row")) {
-    next.classList.toggle("d-none");
-    next = next.nextElementSibling;
+
+  function toggleNested(row) {
+    const icon = row.querySelector('.arrow-icon');
+    if (icon) icon.classList.toggle('rotate');
+
+    let nextRow = row.nextElementSibling;
+    while (nextRow && nextRow.classList.contains('nested-row')) {
+      nextRow.classList.toggle('d-none');
+      nextRow = nextRow.nextElementSibling;
+    }
   }
-}
+
